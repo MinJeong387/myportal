b143ee3cd3cc46659682352a37c40ac00d58ccd3
서민정 <ybsm0629@gmail.com>
2025-01-14 14:30:52 +0900

    commit

---
+++ src/main/java/himedia/myportal/exceptions/ApplicationExceptionHandler.java
@@ -0,0 +1,9 @@
+package himedia.myportal.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+// Global Exception Handler
+@ControllerAdvice
+public class ApplicationExceptionHandler {
+
+}
