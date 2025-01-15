9fc81addb91820908f7a89783e7d80feb5e81492
서민정 <ybsm0629@gmail.com>
2025-01-15 15:09:39 +0900

    users

---
+++ src/main/java/himedia/myportal/controllers/UsersController.java
@@ -0,0 +1,21 @@
+package himedia.myportal.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UsersController {
+
+	@GetMapping({ "", "/", "/join" })
+	public String joinForm() {
+		return "users/joinform";
+	}
+
+	@GetMapping("/joinsuccess")
+	public String joinSuccess() {
+		return "users/joinsuccess";
+	}
+
+}
---
+++ src/main/java/himedia/myportal/repositories/UserDaoImpl.java
@@ -0,0 +1,36 @@
+package himedia.myportal.repositories;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import himedia.myportal.exceptions.UserDaoException;
+import himedia.myportal.repositories.vo.UserVo;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+	@Autowired
+	SqlSession sqlSession;
+
+	@Override
+	public int insert(UserVo vo) {
+		try {
+			return sqlSession.insert("user.insert", vo);
+		} catch (Exception e) {
+			throw new UserDaoException("회원가입 중 오류", vo);
+		}
+	}
+
+	@Override
+	public UserVo selectUser(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserVo selectUser(String email, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
